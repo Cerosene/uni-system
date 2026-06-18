b20ff6e3caddefa493bceaf397151fdc1e03a14b
Mateusz Lewandowski <matlewsp4@wp.pl>
2026-06-18 09:45:04 +0200

    bug fix

--- Usos 2.0/client/src/main/java/pl/usos2/client/service/remote/RemoteApplicationContext.java
+++ Usos 2.0/client/src/main/java/pl/usos2/client/service/remote/RemoteApplicationContext.java
@@ -3,7 +3,9 @@ package pl.usos2.client.service.remote;
 import pl.usos2.client.network.ApiClient;
 import pl.usos2.client.session.ClientSession;
 import pl.usos2.server.config.ApplicationContext;
+import pl.usos2.server.dao.schedule.JdbcScheduleDao;
 import pl.usos2.server.service.audit.AuditLogService;
+import pl.usos2.server.service.schedule.ScheduleService;
 
 public class RemoteApplicationContext extends ApplicationContext {
     public RemoteApplicationContext(ApiClient apiClient, ClientSession session) {
@@ -17,7 +19,8 @@ public class RemoteApplicationContext extends ApplicationContext {
                 new RemoteEmployeeService(apiClient, session),
                 new RemoteServiceTicketService(apiClient, session),
                 new RemoteRentalService(apiClient, session),
-                new RemoteCourseService(apiClient, session)
+                new RemoteCourseService(apiClient, session),
+                new ScheduleService(new JdbcScheduleDao())
         );
     }
 }
